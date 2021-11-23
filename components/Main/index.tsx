0352d677bea1d3fa013b3641f228c78f1fd5fc1f
Luis Lucas <luis@handcom.com.br>
2021-11-22 23:02:37 -0300

    Create Boilerplate

---
+++ components/Main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main>
+      <h1>React Advanced</h1>
+    </main>
+  )
+}
+
+export default Main
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import 'tailwindcss/tailwind.css'
 import { AppProps } from 'next/app'
+import React from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Next.js Tailwind CSS</title>
+      <meta name="descripttion"
+        content="A simple project to work with Typescript, React, Next and Tailwind" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
